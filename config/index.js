700b1f8da34b261464e0232d43521118c195cd40
nicola <nekoulahaddad@gmail.com>
2020-08-13 22:57:40 +0300

    new version

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI, 
-  JWT_SECRET: process.env.JWT_SECRET
+  MONGO_URI: "mongodb+srv://nekoula:Bigsur@cluster0.t9iei.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
+  JWT_SECRET: process.env.JWT_SECRET,
+  PORT: process.env.PORT
 };
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const users = require("./routes/Users");
 const auth = require("./routes/auth");
 const path = require('path');
 const cors = require('cors');
+const PORT = require("./config/index").PORT
 
 const app = express();
 
 const db = require("./config/index").MONGO_URI
+//`${MONGO_URI}/${MONGO_DB_NAME}`;
 
 app.use(express.json());
 
@@ -27,21 +29,21 @@ app.use('/uploads', express.static('uploads'));
 app.use(cors()) // hie mnshan ma yetla3 cors errors (7eyalla sha5es ye2der yfoot 3la al api)
 
 
+
 if (process.env.NODE_ENV === 'production') {
-	app.use('*',(req,res) => {
-		res.sendFile(path.resolve(__dirname,'myapp','build','index.html'));
-	})
+  // Set static folder
+  app.use(express.static('myapp/build'));
 
-
-
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'myapp', 'build', 'index.html'));
+  });
 }
 
 
 
 
-
-const port = process.env.PORT || 5000;
+const port = PORT || 5000;
 
 app.listen(port, () => {
-    console.log("every thing is okk ...");
+    console.log(`every thing is okk ...${port} ${process.env.NODE_ENV}`);
 });
